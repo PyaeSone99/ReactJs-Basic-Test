81e7694f45818c177e6ac560acfe9ece2d6248bb
Pyae Sone Aung <pyaesoneaungbb99@gmail.com>
2023-04-09 21:47:14 +0630

    Basic Components and State

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import User from "./components/users/User";
+import React,{useState,useEffect} from "react";
+import AddUser from "./components/users/AddUser";
 
 function App() {
+  let [users,setUsers] = useState([]);
+  let [showForm,setShowForm] = useState(false);
+  useEffect(()=> {
+    fetch("https://randomuser.me/api/?results=10")
+    .then(res => res.json())
+    .then(users => {
+      let rawUser = users.results;
+      let filterUser = rawUser.map(usr => {
+        return {
+          uuid : usr.login.uuid,
+          name : `${usr.name.title} ${usr.name.first} ${usr.name.last}`,
+          phone : usr.phone,
+          cell : usr.cell,
+          image : usr.picture.thumbnail
+        }
+      });
+      setUsers(filterUser);
+    })
+    .catch(err => console.log(err));
+  },[])
+
+  const removeUserHandler = (uuid)=>{
+      let remainUser = users.filter(ele => ele.uuid !== uuid);
+      setUsers(remainUser);
+  }
+
+  const showUserSubmitForm = ()=>{
+    setShowForm(!showForm);
+  }
+
+  const addUserHandler = (user)=>{
+    let newUserList = [user,...users];
+    setUsers(newUserList);
+    setShowForm(!showForm);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container my-3">
+      <h1 className="text-center my-5 text-info">Our Employee</h1>
+      <button className="btn btn-success my-3" onClick={showUserSubmitForm}>Add Employee</button>
+      {showForm && <AddUser addUser={addUserHandler} />}
+      {
+        users.map( usr => <User data={usr} key={usr.uuid} remove={removeUserHandler} />)
+      }
     </div>
   );
 }
